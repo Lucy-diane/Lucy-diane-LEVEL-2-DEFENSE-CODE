b6806e8fdf0787e1b7770c158b5c5754f215ccc0
Lucy-diane <dammydiane@gmail.com>
2024-08-13 08:53:56 +0100

    added new files

---
+++ VIEW/script.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: '#app',
+    data: {
+        items: [
+            { name: 'ERU', description: 'Combination of Alfredo sauce and two cheeses.', price: 2000, image: 'eru.jpeg' },
+            { name: 'ACHU', description: 'Juicy grilled chicken burger.', price: 2000, image: '5.jpeg' },
+            { name: 'NAME', description: 'Some item description here.', price: 2000, image: '7.jpeg' }
+        ]
+    }
+});
